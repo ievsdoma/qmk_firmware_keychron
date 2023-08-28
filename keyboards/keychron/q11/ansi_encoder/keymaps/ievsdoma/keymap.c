3dedfe92f7ede8d35f8e03ffaaf3bdf7b00e0de0
ievsdoma <54540155+ievsdoma@users.noreply.github.com>
2023-08-28 21:42:48 +0100

    System layer assigned to double Fn hold

--- keyboards/keychron/q11/ansi_encoder/keymaps/ievsdoma/keymap.c
+++ keyboards/keychron/q11/ansi_encoder/keymaps/ievsdoma/keymap.c
@@ -20,53 +20,151 @@ enum layers{
     MAC_FN,
     WIN_BASE,
     WIN_FN,
+    SYSTEM_CMD,
 };
-
-#define SPLIT_WATCHDOG_ENABLE
-#define SPLIT_WATCHDOG_TIMEOUT 7000
 
 #define KC_TASK LGUI(KC_TAB)
 #define KC_FLXP LGUI(KC_E)
 
+// Define a type for as many tap dance states as you need
+typedef enum {
+    TD_NONE,
+    TD_UNKNOWN,
+    TD_SINGLE_TAP,
+    TD_SINGLE_HOLD,
+    TD_DOUBLE_TAP,
+    TD_DOUBLE_HOLD,
+} td_state_t;
+
+typedef struct {
+    bool is_press_action;
+    td_state_t state;
+} td_tap_t;
+
+enum {
+    TWO_LAYR, // Our custom tap dance key; add any other tap dance keys to this enum
+};
+
+// Declare the functions to be used with your tap dance key(s)
+
+// Function associated with all tap dances
+td_state_t cur_dance(tap_dance_state_t *state);
+
+// Functions associated with individual tap dances
+void ql_finished(tap_dance_state_t *state, void *user_data);
+void ql_reset(tap_dance_state_t *state, void *user_data);
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_91_ansi(
-        KC_MUTE,  KC_ESC,   KC_BRID,  KC_BRIU,     KC_MCTL,  KC_LPAD,  RGB_VAD,   RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  KC_INS,   KC_DEL,   KC_MUTE,
-        _______,  KC_GRV,   KC_1,     KC_2,        KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
-        _______,  KC_TAB,   KC_Q,     KC_W,        KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
-        _______,  KC_CAPS,  KC_A,     KC_S,        KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
-        _______,  KC_LSFT,            KC_Z,        KC_X,     KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-        _______,  KC_LCTL,  KC_LOPT,  KC_LCMD,     MO(MAC_FN),         KC_SPC,                        KC_SPC,             KC_RCMD,  MO(MAC_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_MUTE,  KC_ESC,   KC_BRID,  KC_BRIU,     KC_MCTL,  KC_LPAD,    RGB_VAD,   RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,      KC_VOLU,  KC_INS,   KC_DEL,   KC_MUTE,
+        _______,  KC_GRV,   KC_1,     KC_2,        KC_3,     KC_4,       KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,      KC_EQL,   KC_BSPC,            KC_PGUP,
+        _______,  KC_TAB,   KC_Q,     KC_W,        KC_E,     KC_R,       KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,      KC_RBRC,  KC_BSLS,            KC_PGDN,
+        _______,  KC_CAPS,  KC_A,     KC_S,        KC_D,     KC_F,       KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                KC_ENT,             KC_HOME,
+        _______,  KC_LSFT,            KC_Z,        KC_X,     KC_C,       KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,                KC_RSFT,  KC_UP,
+        _______,  KC_LCTL,  KC_LOPT,  KC_LCMD,     MO(MAC_FN),           KC_SPC,                        KC_SPC,             KC_RCMD,  MO(MAC_FN),   KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [MAC_FN] = LAYOUT_91_ansi(
-        RGB_TOG,  _______,  KC_F1,    KC_F2,       KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   _______,  _______,  RGB_TOG,
-        _______,  _______,  _______,  _______,     _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
-        _______,  RGB_TOG,  RGB_MOD,  RGB_VAI,     RGB_HUI,  RGB_SAI,  RGB_SPI,   _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
-        _______,  _______,  RGB_RMOD, RGB_VAD,     RGB_HUD,  RGB_SAD,  RGB_SPD,   _______,  _______,  _______,  _______,  _______,  _______,              _______,            _______,
-        _______,  _______,            _______,     _______,  _______,  _______,   _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,  _______,
-        _______,  _______,  _______,  _______,     _______,            _______,                       _______,            _______,  _______,    _______,  _______,  _______,  _______),
+        RGB_TOG,  _______,  KC_F1,    KC_F2,       KC_F3,    KC_F4,      KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,       KC_F12,   _______,  _______,  RGB_TOG,
+        _______,  _______,  _______,  _______,     _______,  _______,    _______,   _______,  _______,  _______,  _______,  _______,  _______,      _______,  _______,            _______,
+        _______,  RGB_TOG,  RGB_MOD,  RGB_VAI,     RGB_HUI,  RGB_SAI,    RGB_SPI,   _______,  _______,  _______,  _______,  _______,  _______,      _______,  _______,            _______,
+        _______,  _______,  RGB_RMOD, RGB_VAD,     RGB_HUD,  RGB_SAD,    RGB_SPD,   _______,  _______,  _______,  _______,  _______,  _______,                _______,            _______,
+        _______,  _______,            _______,     _______,  _______,    _______,   _______,  NK_TOGG,  _______,  _______,  _______,  _______,                _______,  _______,
+        _______,  _______,  _______,  _______,     _______,              _______,                       _______,            _______,  _______,      _______,  _______,  _______,  _______),
 
     [WIN_BASE] = LAYOUT_91_ansi(
-        KC_CALC,  KC_ESC,   KC_F1,    KC_F2,       KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_PSCR,  KC_DEL,   KC_MUTE,
-        KC_HOME,  KC_GRV,   KC_1,     KC_2,        KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
-        KC_DEL,   KC_TAB,   KC_Q,     KC_W,        KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_END,
-        KC_PGUP,  CW_TOGG,  KC_A,     KC_S,        KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_PGDN,
-        KC_PGDN,  KC_LSFT,            KC_Z,        KC_X,     KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-        KC_END,   KC_LCTL,  KC_LWIN,  MO(WIN_FN),  KC_LALT,            KC_SPC,                        KC_SPC,             KC_RALT,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_CALC,  KC_ESC,   KC_F1,    KC_F2,         KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,       KC_F12,   KC_PSCR,  KC_DEL,   KC_MUTE,
+        KC_HOME,  KC_GRV,   KC_1,     KC_2,          KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,      KC_EQL,   KC_BSPC,            KC_PGUP,
+        KC_DEL,   KC_TAB,   KC_Q,     KC_W,          KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,      KC_RBRC,  KC_BSLS,            KC_END,
+        KC_PGUP,  CW_TOGG,  KC_A,     KC_S,          KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                KC_ENT,             KC_PGDN,
+        KC_PGDN,  KC_LSFT,            KC_Z,          KC_X,     KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,                KC_RSFT,  KC_UP,
+        KC_END,   KC_LCTL,  KC_LWIN,  TD(TWO_LAYR),  KC_LALT,            KC_SPC,                        KC_SPC,             KC_RALT,  TD(TWO_LAYR), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [WIN_FN] = LAYOUT_91_ansi(
-        RGB_TOG,  _______,  KC_BRID,  KC_BRIU,     KC_TASK,  KC_FLXP,  RGB_VAD,   RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  QK_BOOT,  EE_CLR,   RGB_TOG,
-        _______,  _______,  _______,  _______,     _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
-        AS_RPT,   _______,  QK_BOOT,  _______,     KC_7,     KC_8,     KC_9,      RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,    _______,  _______,            _______,
-        AS_UP,    KC_CAPS,  _______,  _______,     KC_4,     KC_5,     KC_6,      RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,              _______,            _______,
-        AS_DOWN,  AS_TOGG,            _______,     KC_0,     KC_1,     KC_2,      KC_3,     NK_TOGG,  _______,  _______,  _______,  _______,              _______,  _______,
-        _______,  _______,  _______,  _______,     _______,            _______,                       _______,            _______,  _______,    _______,  _______,  _______,  _______),
+        RGB_TOG,  _______,  KC_BRID,  KC_BRIU,     KC_TASK,  KC_FLXP,    RGB_VAD,   RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,      KC_VOLU,  _______,  _______,  RGB_TOG,
+        _______,  _______,  _______,  _______,     _______,  _______,    _______,   _______,  _______,  _______,  _______,  _______,  _______,      _______,  _______,            _______,
+        AS_RPT,   _______,  _______,  _______,     KC_7,     KC_8,       KC_9,      RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,      _______,  _______,            _______,
+        AS_UP,    KC_CAPS,  _______,  _______,     KC_4,     KC_5,       KC_6,      RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,                _______,            _______,
+        AS_DOWN,  AS_TOGG,            _______,     KC_0,     KC_1,       KC_2,      KC_3,     NK_TOGG,  _______,  _______,  _______,  _______,                _______,  _______,
+        _______,  _______,  _______,  _______,     _______,              _______,                       _______,            _______,  _______,      _______,  _______,  _______,  _______),
+
+    [SYSTEM_CMD] = LAYOUT_91_ansi(
+        QK_BOOT,  _______,  _______,  _______,     _______,  _______,    _______,   _______,  _______,  _______,  _______,  _______,  _______,      _______,  _______,  _______,  QK_BOOT,
+        _______,  _______,  _______,  _______,     _______,  _______,    _______,   _______,  _______,  _______,  _______,  _______,  _______,      _______,  _______,            _______,
+        _______,  _______,  _______,  _______,     _______,  _______,    _______,   _______,  _______,  _______,  _______,  _______,  _______,      _______,  _______,            _______,
+        _______,  _______,  _______,  _______,     _______,  _______,    _______,   _______,  _______,  _______,  _______,  _______,  _______,                _______,            _______,
+        QK_RBT,   _______,            _______,     _______,  _______,    _______,   _______,  _______,  _______,  _______,  _______,  _______,                _______,  _______,
+        EE_CLR,   _______,  _______,  _______,     _______,              _______,                       _______,            _______,  _______,      _______,  _______,  _______,  _______),
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [MAC_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [MAC_FN]   = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(RGB_VAD, RGB_VAI) },
-    [WIN_BASE] = { ENCODER_CCW_CW(KC_PGUP, KC_PGDN), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [WIN_FN]   = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(RGB_RMOD, RGB_MOD) }
+    [MAC_BASE]   = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [MAC_FN]     = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(RGB_VAD, RGB_VAI) },
+    [WIN_BASE]   = { ENCODER_CCW_CW(KC_PGUP, KC_PGDN), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [WIN_FN]     = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(RGB_RMOD, RGB_MOD) },
+    [SYSTEM_CMD] = { ENCODER_CCW_CW(RGB_HUD, RGB_HUI), ENCODER_CCW_CW(RGB_SPD, RGB_SPI) },
 };
 #endif // ENCODER_MAP_ENABLE
+
+// Determine the current tap dance state
+td_state_t cur_dance(tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (!state->pressed) return TD_SINGLE_TAP;
+        else return TD_SINGLE_HOLD;
+    } else if (state->count == 2) {
+        if (!state->pressed) return TD_DOUBLE_TAP;
+        else return TD_DOUBLE_HOLD;
+    }
+    else return TD_UNKNOWN;
+}
+
+// Initialize tap structure associated with example tap dance key
+static td_tap_t ql_tap_state = {
+    .is_press_action = true,
+    .state = TD_NONE
+};
+
+// Functions that control what our tap dance key does
+void ql_finished(tap_dance_state_t *state, void *user_data) {
+    ql_tap_state.state = cur_dance(state);
+    switch (ql_tap_state.state) {
+        case TD_SINGLE_TAP:
+            break;
+        case TD_SINGLE_HOLD:
+            layer_on(WIN_FN);
+            break;
+        case TD_DOUBLE_TAP:
+            break;
+        case TD_DOUBLE_HOLD:
+            layer_on(SYSTEM_CMD);
+            break;
+        default:
+            break;
+    }
+}
+
+void ql_reset(tap_dance_state_t *state, void *user_data) {
+    // If the key was held down and now is released then switch off the layer
+    if (ql_tap_state.state == TD_SINGLE_HOLD) {
+        layer_off(WIN_FN);
+    }
+    if (ql_tap_state.state == TD_DOUBLE_HOLD) {
+        layer_off(SYSTEM_CMD);
+    }
+    ql_tap_state.state = TD_NONE;
+}
+
+// Associate our tap dance key with its functionality
+tap_dance_action_t tap_dance_actions[] = {
+    [TWO_LAYR] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, ql_finished, ql_reset)
+};
+
+// Set a long-ish tapping term for tap-dance keys
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case QK_TAP_DANCE ... QK_TAP_DANCE_MAX:
+            return 500;
+        default:
+            return TAPPING_TERM;
+    }
+}
--- keyboards/keychron/q11/config.h
+++ keyboards/keychron/q11/config.h
@@ -58,3 +58,6 @@
 // https://docs.qmk.fm/#/feature_rgb_matrix?id=rgb-matrix-effects
 #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 #define RGB_MATRIX_KEYPRESSES
+
+#define SPLIT_WATCHDOG_ENABLE
+#define SPLIT_WATCHDOG_TIMEOUT 7000
