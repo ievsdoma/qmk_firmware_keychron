26d7d33bcc2edb096e7862fde2541095ba3f8ca3
ievsdoma <54540155+ievsdoma@users.noreply.github.com>
2023-09-06 22:03:05 +0100

    Indicators: CAPS words and simple layers

--- keyboards/keychron/q11/ansi_encoder/keymaps/ievsdoma/keymap.c
+++ keyboards/keychron/q11/ansi_encoder/keymaps/ievsdoma/keymap.c
@@ -13,6 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include <keymap.h>
 #include QMK_KEYBOARD_H
 
 enum layers{
@@ -88,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  _______,  _______,     _______,              _______,                       _______,            _______,  _______,      _______,  _______,  _______,  _______),
 
     [SYSTEM_CMD] = LAYOUT_91_ansi(
-        QK_BOOT,  _______,  _______,  _______,     _______,  _______,    _______,   _______,  _______,  _______,  _______,  _______,  _______,      _______,  _______,  _______,  QK_BOOT,
+        _______,  QK_BOOT,  _______,  _______,     _______,  _______,    _______,   _______,  _______,  _______,  _______,  _______,  _______,      _______,  _______,  QK_BOOT,  _______,
         _______,  _______,  _______,  _______,     _______,  _______,    _______,   _______,  _______,  _______,  _______,  _______,  _______,      _______,  _______,            _______,
         _______,  _______,  _______,  _______,     _______,  _______,    _______,   _______,  _______,  _______,  _______,  _______,  _______,      _______,  _______,            _______,
         _______,  _______,  _______,  _______,     _______,  _______,    _______,   _______,  _______,  _______,  _______,  _______,  _______,                _______,            _______,
@@ -167,4 +168,30 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
         default:
             return TAPPING_TERM;
     }
+}
+
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    if (get_highest_layer(layer_state) > 0) {
+        uint8_t layer = get_highest_layer(layer_state);
+
+        for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
+            for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
+                uint8_t index = g_led_config.matrix_co[row][col];
+
+                if (index >= led_min && index < led_max && index != NO_LED && keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
+                    rgb_matrix_set_color(index, RGB_PURPLE);
+                }
+            }
+        }
+    }
+
+    if(is_caps_word_on()){
+        RGB_MATRIX_INDICATOR_SET_COLOR(16, 255, 0, 0);
+        RGB_MATRIX_INDICATOR_SET_COLOR(22, 255, 0, 0);
+        RGB_MATRIX_INDICATOR_SET_COLOR(23, 255, 0, 0);
+        RGB_MATRIX_INDICATOR_SET_COLOR(24, 255, 0, 0);
+        RGB_MATRIX_INDICATOR_SET_COLOR(30, 255, 0, 0);
+    }
+
+    return true;
 }
---
+++ keyboards/keychron/q11/ansi_encoder/keymaps/ievsdoma/keymap.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define AUTO_SHIFT_TIMEOUT 150
+#define CAPS_WORD_INVERT_ON_SHIFT
+#define CAPS_WORD_IDLE_TIMEOUT 3000
+
+void set_caps_indicators(uint8_t red, uint8_t green, uint8_t blue);
