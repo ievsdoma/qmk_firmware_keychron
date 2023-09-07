d65308b251cfc5568b68e7d8866107b998ed566f
ievsdoma <54540155+ievsdoma@users.noreply.github.com>
2023-09-07 21:02:40 +0100

    config.h added, settings migrated

--- keyboards/keychron/q11/ansi_encoder/keymaps/ievsdoma/config.h
+++ keyboards/keychron/q11/ansi_encoder/keymaps/ievsdoma/config.h
@@ -1,7 +1,8 @@
 #pragma once
 
+#define SPLIT_WATCHDOG_ENABLE
+#define SPLIT_LAYER_STATE_ENABLE
+
 #define AUTO_SHIFT_TIMEOUT 150
 #define CAPS_WORD_INVERT_ON_SHIFT
 #define CAPS_WORD_IDLE_TIMEOUT 3000
-
-void set_caps_indicators(uint8_t red, uint8_t green, uint8_t blue);
