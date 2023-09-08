b22780e95fada545db0868b342a26e7331f1773a
ievsdoma <54540155+ievsdoma@users.noreply.github.com>
2023-09-08 19:25:15 +0100

    Layer indicators will be cleared only once

--- keyboards/keychron/q11/ansi_encoder/keymaps/ievsdoma/keymap.c
+++ keyboards/keychron/q11/ansi_encoder/keymaps/ievsdoma/keymap.c
@@ -181,6 +181,7 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
             for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
                 uint8_t index = g_led_config.matrix_co[row][col];
 
+                // set only leds in the range
                 if (index >= led_min && index < led_max && index != NO_LED && keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
                     rgb_matrix_set_color(index, RGB_PURPLE);
                 }
@@ -188,17 +189,20 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
         }
 
         g_last_layer = layer;
+
     } else if (!rgb_matrix_get_flags() && g_last_layer > 0) {
         for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
             for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
                 uint8_t index = g_led_config.matrix_co[row][col];
 
-                if (index >= led_min && index < led_max && index != NO_LED && keymap_key_to_keycode(g_last_layer, (keypos_t){col,row}) > KC_TRNS) {
+                // set all leds at once and forget
+                if (index != NO_LED && keymap_key_to_keycode(g_last_layer, (keypos_t){col,row}) > KC_TRNS) {
                     rgb_matrix_set_color(index, RGB_OFF);
                 }
             }
         }
 
+        g_last_layer = 0;
         continue_led_processing = false;
     }
 
