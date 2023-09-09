cb6c237a633541fbbbfd2c509ed57d658ad10572
ievsdoma <54540155+ievsdoma@users.noreply.github.com>
2023-09-09 10:49:34 +0100

    Caps indicators in layout file

--- users/ievsdoma/ievsdoma.h
+++ users/ievsdoma/ievsdoma.h
@@ -26,3 +26,6 @@ typedef struct {
 enum {
     TWO_LAYR, // Our custom tap dance key; add any other tap dance keys to this enum
 };
+
+const uint8_t* g_caps_indicators;
+const uint8_t g_caps_indicators_count;
