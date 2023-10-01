919a5fd0f15c142208700ef72bdc3be17ba52892
ievsdoma <54540155+ievsdoma@users.noreply.github.com>
2023-10-01 19:18:02 +0100

    Changes from q1 pro integrated

---
+++ users/ievsdoma/indicators.h
@@ -0,0 +1,4 @@
+#pragma once
+
+const uint8_t* g_caps_indicators;
+const uint8_t g_caps_indicators_count;
---
+++ users/ievsdoma/two_layers_tap_dance.h
@@ -0,0 +1,13 @@
+#pragma once
+
+enum layers{
+    MAC_BASE,
+    MAC_FN,
+    WIN_BASE,
+    WIN_FN,
+    SYSTEM_CMD,
+};
+
+enum {
+    TWO_LAYR, // Our custom tap dance key; add any other tap dance keys to this enum
+};
