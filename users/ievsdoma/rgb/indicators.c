3baaf6541b7388e00d2ed9da1b1fde6368f0c4ef
ievsdoma <54540155+ievsdoma@users.noreply.github.com>
2023-10-23 21:08:21 +0100

    Indicators use key highlight

--- users/ievsdoma/rgb/indicators.c
+++ users/ievsdoma/rgb/indicators.c
@@ -1,46 +1,18 @@
 #include QMK_KEYBOARD_H
 #include "indicators.h"
+#include "key_category_highlight.h"
 
 #define RGB_MATRIX_CAPS_INDICATORS_SET_COLOR(r, g, b) \
     for (uint8_t led = 0; led < g_caps_indicators_count; led++) { \
         RGB_MATRIX_INDICATOR_SET_COLOR(g_caps_indicators[led], r, g, b); \
     }
 
-uint8_t last_layer_number = 0;
-
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     bool continue_led_processing = true;
 
     uint8_t layer = get_highest_layer(layer_state);
     if (layer > 0) {
-
-        for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
-            for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
-                uint8_t index = g_led_config.matrix_co[row][col];
-
-                // set only leds in the range
-                if (index >= led_min && index < led_max && index != NO_LED && keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
-                    rgb_matrix_set_color(index, RGB_PURPLE);
-                }
-            }
-        }
-
-        last_layer_number = layer;
-
-    } else if (!rgb_matrix_get_flags() && last_layer_number > 0) {
-        for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
-            for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
-                uint8_t index = g_led_config.matrix_co[row][col];
-
-                // set all leds at once and forget
-                if (index != NO_LED && keymap_key_to_keycode(last_layer_number, (keypos_t){col,row}) > KC_TRNS) {
-                    rgb_matrix_set_color(index, RGB_OFF);
-                }
-            }
-        }
-
-        last_layer_number = 0;
-        continue_led_processing = false;
+        key_category_highlight(layer, led_min, led_max);
     }
 
     if(is_caps_word_on()){
