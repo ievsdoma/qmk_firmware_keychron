5aa8612837503a1b75a36004769fdcb51d8beff6
ievsdoma <54540155+ievsdoma@users.noreply.github.com>
2023-12-20 19:49:21 +0000

    Changes from another branch integrated

--- users/ievsdoma/rgb/key_category_highlight.h
+++ users/ievsdoma/rgb/key_category_highlight.h
@@ -1,3 +1,5 @@
 #pragma once
 
-void key_category_highlight(uint8_t layer, uint8_t led_min, uint8_t led_max);
+typedef HSV (*colour_processor)(HSV input, uint8_t i, uint8_t time);
+
+void key_category_highlight(uint8_t layer, uint8_t led_min, uint8_t led_max, colour_processor colour_processor_func);
