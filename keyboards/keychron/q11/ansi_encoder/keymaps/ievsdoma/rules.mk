00d3c3aab934b24f8c51dcf3dbe6a355ebca5eb9
ievsdoma <54540155+ievsdoma@users.noreply.github.com>
2025-03-09 20:05:51 +0000

    Profile added for Ploopy Mouse

--- keyboards/keychron/q11/ansi_encoder/keymaps/ievsdoma/rules.mk
+++ keyboards/keychron/q11/ansi_encoder/keymaps/ievsdoma/rules.mk
@@ -1,3 +1,11 @@
 INTROSPECTION_KEYMAP_C = features/two_layers_tap_dance.c
+SRC += features/custom_keycodes.c
+SRC += rgb/indicators.c
+SRC += rgb/key_category_highlight.c
 
+ENCODER_MAP_ENABLE = yes
+AUTO_SHIFT_ENABLE = yes
+CAPS_WORD_ENABLE = yes
 TAP_DANCE_ENABLE = yes
+RGB_MATRIX_CUSTOM_USER = yes
+CONSOLE_ENABLE = yes
