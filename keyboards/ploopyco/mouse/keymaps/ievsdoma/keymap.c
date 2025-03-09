a5c49667ea86a17b56b71493bee993af41f75c18
ievsdoma <54540155+ievsdoma@users.noreply.github.com>
2025-03-09 20:05:51 +0000

    Profile added for Ploopy Mouse

---
+++ keyboards/ploopyco/mouse/keymaps/ievsdoma/keymap.c
@@ -0,0 +1,29 @@
+/* Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+ * Copyright 2019 Sunjun Kim
+ * Copyright 2020 Ploopy Corporation
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(/* Base */
+                 KC_BTN4,       KC_BTN1, KC_BTN3, KC_BTN2,      KC_BTN5,        MO(2),      MO(1),      _______),
+    [1] = LAYOUT(/* Base */
+                 C(S(KC_TAB)),  C(KC_C), _______, C(KC_V),      C(KC_TAB),      MO(3),      _______,    _______),
+    [2] = LAYOUT(/* Base */
+                 KC_ESC,        KC_HOME, _______, KC_END,       KC_WWW_REFRESH, _______,    MO(3),      DPI_CONFIG),
+    [3] = LAYOUT(/* Base */
+                 _______,       C(KC_W), _______, C(S(KC_T)),   _______,        _______,    _______,    _______),
+};
--- users/ievsdoma/rules.mk
+++ users/ievsdoma/rules.mk
@@ -1,15 +1,1 @@
-SRC += features/custom_keycodes.c
-# SRC += features/two_layers_tap_dance.c
-SRC += rgb/indicators.c
-SRC += rgb/key_category_highlight.c
-
 VIA_ENABLE = yes
-ENCODER_MAP_ENABLE = yes
-AUTO_SHIFT_ENABLE = yes
-CAPS_WORD_ENABLE = yes
-# TAP_DANCE_ENABLE = yes
-RGB_MATRIX_CUSTOM_USER = yes
-# Uncomment for console
-# CONSOLE_ENABLE = yes
-# VIA_ENABLE = no
-# RAW_ENABLE = no
