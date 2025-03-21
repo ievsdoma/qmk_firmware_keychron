1831ba8ed88628276d360e8d723966a4ebb9ae0f
ievsdoma <54540155+ievsdoma@users.noreply.github.com>
2025-03-21 17:18:47 +0000

    Layer tap hold for mouse

--- keyboards/ploopyco/mouse/keymaps/ievsdoma/keymap.c
+++ keyboards/ploopyco/mouse/keymaps/ievsdoma/keymap.c
@@ -20,13 +20,13 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(/* Base */
-                 KC_BTN4,       KC_BTN1, KC_BTN3, KC_BTN2,      KC_BTN5,        MO(2),      MO(1),      _______),
+                KC_WWW_REFRESH, KC_BTN1, KC_BTN3, KC_BTN2,      RCTL_T(KC_ESC), LT(2, KC_BTN4), LT(1, KC_BTN5), _______),
     [1] = LAYOUT(/* Base */
-                 C(S(KC_TAB)),  C(KC_C), _______, C(KC_V),      C(KC_TAB),      MO(3),      _______,    _______),
+                C(S(KC_TAB)),   C(KC_C), _______, C(KC_V),      C(KC_TAB),      MO(3),          _______,        _______),
     [2] = LAYOUT(/* Base */
-                 KC_ESC,        KC_HOME, _______, KC_END,       KC_WWW_REFRESH, _______,    MO(3),      DPI_CONFIG),
+                C(S(KC_T)),     KC_HOME, _______, KC_END,       C(KC_W),        _______,        MO(3),          DPI_CONFIG),
     [3] = LAYOUT(/* Base */
-                 _______,       C(KC_W), _______, C(S(KC_T)),   _______,        _______,    _______,    QK_BOOT),
+                _______,        C(KC_W), _______, C(S(KC_T)),   _______,        _______,        _______,        QK_BOOT),
 };
 
 #if defined(ENCODER_MAP_ENABLE)
@@ -39,8 +39,20 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 #endif // ENCODER_MAP_ENABLE
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (!process_record_custom(keycode, record)) {
-        return false;
+    switch (keycode) {
+        case LT(2, KC_BTN4):
+            if (record->tap.count && record->event.pressed) {
+                tap_code16(KC_BTN4);
+                return false;
+            }
+            return true;
+        case LT(1, KC_BTN5):
+            if (record->tap.count && record->event.pressed) {
+                tap_code16(KC_BTN5);
+                return false;
+            }
+            return true;
     }
-    return true;
+
+     return process_record_custom(keycode, record);
 }
